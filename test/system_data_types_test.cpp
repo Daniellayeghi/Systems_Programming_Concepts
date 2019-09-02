528746904ef611ac87f14299790c25eef3c3d2dc
Daniellayeghi <Daniellayeghi@gmail.com>
2019-09-02 13:32:07 +0430

    Testing structure cache alignment

--- test/system_data_types_test.cpp
+++ test/system_data_types_test.cpp
@@ -19,10 +19,10 @@ public:
 TEST_F(SystemsDataTypes, Struct_Byte_Granularity)
 {
     /**
-  * @desc The pack command gives a minimum granularity of 1 byte to the compiler.
-  * This is in order to stop the compiler to optimize the uint16_t to uint64_t,
-  * consecutively increasing the struct size to 16 (instead of 10).
-*/
+    * @desc The pack command gives a minimum granularity of 1 byte to the compiler.
+    * This is in order to stop the compiler to optimize the uint16_t to uint64_t,
+    * consecutively increasing the struct size to 16 (instead of 10).
+    */
 
     #pragma pack(push, 1)
     struct MyStruct {
@@ -35,19 +35,19 @@ TEST_F(SystemsDataTypes, Struct_Byte_Granularity)
 }
 
 
-TEST_F(SystemsDataTypes, Struct_Alignment_Using_Padding)
+TEST_F(SystemsDataTypes, Struct_Byte_Alignment_Using_Padding)
 {
     /**
-  * @desc By manually reordering the members of MyStruct we decrease alignment requirement
-  * and halve the size of the struct. The compiler adds padding to meet alignment as such:
-  * struct MyStruct {
-  *     char byte_1;
-  *     char pad[7]
-  *     int *byte_8;
-  *     short byte_2;
-  *     char pad[6]
-  *};
-*/
+    * @desc By manually reordering the members of MyStruct we decrease alignment requirement
+    * and halve the size of the struct. The compiler adds padding to meet alignment as such:
+    * struct MyStruct {
+    *     char byte_1;
+    *     char pad[7]
+    *     int *byte_8;
+    *     short byte_2;
+    *     char pad[6]
+    *};
+    */
 
     struct MyStruct {
         char byte_1;
@@ -67,13 +67,50 @@ TEST_F(SystemsDataTypes, Struct_Alignment_Using_Padding)
 }
 
 
+TEST_F(SystemsDataTypes, Struct_Cache_Alignment) {
+    /**
+    * @desc The pack command gives a minimum granularity of 1 byte to the compiler
+     * creating a byte aligned struct. However this does not guarantee that the structure
+     * is also cache aligned. Using alignas we can enforce this.
+    */
+
+    #pragma pack(push, 1)
+    struct MyStruct {
+        std::uint16_t value_1 = 0; //2 bytes
+        std::uint64_t value_2 = 0; //8 bytes
+    };
+    #pragma pack(pop)
+
+
+    #pragma pack(push, 1)
+    struct alignas(16) MyStructCacheAligned {
+        std::uint16_t value_1 = 0; //2 bytes
+        std::uint64_t value_2 = 0; //8 bytes
+    };
+    #pragma pack(pop)
+
+
+    MyStruct instance;
+    MyStructCacheAligned cache_aligned_instance;
+
+    auto instance_address = reinterpret_cast<uintptr_t> (&instance);
+    auto nibble = instance_address % 16;
+
+    auto cache_aligned_instance_address = reinterpret_cast<uintptr_t> (&cache_aligned_instance);
+    auto cache_aligned_nibble = cache_aligned_instance_address % 16;
+
+    ASSERT_TRUE(nibble != 0);
+    ASSERT_TRUE(cache_aligned_nibble == 0);
+}
+
+
 TEST_F(SystemsDataTypes, Internal_Linkage_in_Namespace)
 {
     /**
-  * @desc This test verifies the effects of internal linkage. The static struct
-  * in the sys_prog.h is unique to the translation unit. Therefor the instance
-  * used in sys_prog.cpp is different to the one here. the increments below show this.
-*/
+    * @desc This test verifies the effects of internal linkage. The static struct
+    * in the sys_prog.h is unique to the translation unit. Therefor the instance
+    * used in sys_prog.cpp is different to the one here. the increments below show this.
+    */
 
     using namespace SystemProgrammingPractice::SystemTypes;
 
